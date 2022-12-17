46a280f91a58cb95945ede9fa7af648fa740880e
Kazeke <kazbek_kadirov@mail.ru>
2022-12-17 19:00:44 +0500

    3

--- D1/1.js
+++ D1/1.js
@@ -1,1 +1,98 @@
-console.log('hello');
+//console.log('Hello World');
+// Однострочный Комментарии
+
+/*
+Многострочный комментарий  
+*/
+
+//Переменные
+//c = x + y;
+
+/*let x = 50;
+let y = 10;
+let z = x + y;
+console.log(z);
+
+z = x - y;
+console.log(z);
+z = x * y;
+console.log(z);
+z = x / y;
+console.log(z);
+z = x % y;
+console.log(z);
+
+z = 12 % 13;
+console.log(z);
+
+const PI = 3.14;
+
+
+let x, y, z;
+
+x = 3;
+y = 27;
+z = 44;
+
+console.log(x ** 5);
+console.log(10 ** 5);
+
+let result = x * y + z / x + y / z * x;
+console.log(result);
+
+
+let day = 24;
+let minut = 60;
+let second = 60;
+let secondDay = day * minut * second;
+console.log(secondDay);
+
+let chislo = 23; //number - целочисленная
+let imya = 'Magzhan'; //string - строковая
+let PI = 3.14; //float - с плав точкой
+let onVret = true; //boolean - логическая
+null //Ничего
+NaN // Not a Number - не число
+undefined // Не найдено или не определено
+infinity // Бесконечность
+
+
+let myname, surname, fathername;
+myname = 'Kazbek';
+surname = 'Kadyrov';
+fathername = 'Fayzildaevich';
+
+// переменная + ' ' + переменная
+let FIO = myname + ' ' + surname + ' ' + fathername;
+console.log(FIO);
+
+
+let v = 5;
+let q = "5";
+console.log(typeof v);
+console.log(typeof q);
+console.log(v + +q);
+v = 100;
+console.log(v);
+
+v = v + 5;
+v += 5; //v = v + 5;
+v -= 5; //v = v - 5;
+v *= 5; //v = v * 5;
+v /= 5; //v = v / 5;
+
+q = +q; // перезапись
+console.log(v + q);
+console.log(typeof q);
+let j = +q;
+j = Number(q)
+
+console.log(v + j);
+*/
+
+let login = "Kazeke123";
+let loginBD = "Kazeke123";
+let v = 5;
+let q = "5";
+q = +q;
+v === q ? console.log("Доступ разрещен") : console.log("Доступ запрещен");
